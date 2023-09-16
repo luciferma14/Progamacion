bda08bd7eb50cdde812b7abfd21b4397d09fffc6
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-09-16 18:35:49 +0200

    Add files via upload

---
+++ Ejercicio23.java
@@ -0,0 +1,21 @@
+public class Ejercicio23.java
+{
+    public static void main(String[] args);
+    {    // a=8, b=3, c=-5
+    int a = 8;
+    int b = 3;
+    int c = -5;
+
+    System.out.println(a + b + c);
+     System.out.println(2 * b + 3 * (a - c));
+      System.out.println(a / b);
+       System.out.println(a % b);
+        System.out.println(a / c);
+         System.out.println(a % c);
+          System.out.println(a * b / c);
+           System.out.println(a * (b / c));
+            System.out.println(a * c % b);
+             System.out.println((a - 3 * b) % (c + 2 * a) / (a - c));
+              System.out.println(a - b - c * 2);
+    }
+}
