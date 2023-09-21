0f6d35a32d1c0ae5f9e8ca6a77a3ae06fc2e3c2f
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-09-21 16:53:48 +0200

    Add files via upload

---
+++ EjerciciosBasicos2/Ejercicio6.java
@@ -0,0 +1,14 @@
+public class Ejercicio6 {
+    public static void main(String[] args) {
+
+        String nombre = "Lucia Ferrandis Martinez";
+
+        int Lucia =  nombre.indexOf(0);
+
+
+        System.out.println(Lucia);
+
+
+        
+    }
+}
