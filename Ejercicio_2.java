244f499855725792ec1a5a737cf4cedb6049e0cc
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-09-22 17:28:40 +0200

    Add files via upload

---
+++ Ejercicio_2.java
@@ -0,0 +1,12 @@
+ public class Ejercicio_2 {
+    public static void main(String[] args) {
+        System.out.println("Peso: ");
+        System.out.println("Altura: ");
+
+        int numeroPeso = scanner.nextInt();
+        int numeroAltura = scanner.nextInt();
+
+            System.out.println();
+    }
+
+}
