07041ad9d9c1f376439f61887541ea5d843c8420
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-09-24 18:32:46 +0200

    Add files via upload

--- Ejercicio_2.java
+++ Ejercicio_2.java
@@ -4,21 +4,26 @@ import java.util.Scanner;
 
 public class Ejercicio_2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-         System.out.println(scanner);
+        try (Scanner teclado = new Scanner(System.in)) {
+            System.out.println("Peso: "); // En kilogramos
+            double numeroPeso = teclado.nextDouble();
+            System.out.println("Altura: "); // En metros
+            float numeroAltura = teclado.nextFloat();
 
-        System.out.println("Peso: "); // En kilogramos
-        System.out.println("Altura: "); // En metros
+             double imc = (numeroPeso/(numeroAltura*numeroAltura));
+             System.out.println("Tu índice de masa corporal es: " + imc);
 
-        double numeroPeso = scanner.nextDouble();
-        double numeroAltura = scanner.nextDouble();
-        double imc = (numeroPeso/numeroAltura*numeroAltura);
-
+            if (imc < 18.5) {
+                System.out.println("Bajo peso");
+            } else if (imc >= 18.5 && imc <= 24.9) {
+                System.out.println("Normal");
+            } else if (imc >= 25 && imc <= 29.9) {
+                System.out.println("Sobrepeso");
+            } else if (imc >= 30 && imc <= 30.01) {
+                System.out.println("Obesidad");
+            }
+        }
          
-         System.out.println("\nTu índice de masa corporal es: " + imc);
-         
-
-        
     }
 
 }
