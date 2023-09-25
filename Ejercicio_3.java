defd98b72ba9bff0f8e87104e01895ee9f161947
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-09-25 18:13:01 +0200

    Add files via upload

---
+++ Ejercicio_3.java
@@ -0,0 +1,20 @@
+ import java.lang.Math;
+ 
+ public class Ejercicio_3  {
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 2;
+        int c = -2;
+    
+        Math.sqrt(20);
+        int x = 3;
+        boolean x1 = a*(x*x)+b*x+c==0;
+        boolean x2 = a*(x*x)-b*x+c==0;
+        System.out.println(x1);
+        System.out.println(x2);
+
+        
+    }
+
+}
