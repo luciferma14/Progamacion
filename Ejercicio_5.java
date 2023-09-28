86d1330f444c0c6b3487f87b21207a584b8dcfcb
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-09-28 11:39:58 +0200

    Add files via upload

---
+++ Ejercicio_5.java
@@ -0,0 +1,22 @@
+package EjerciciosAvanzados1;
+import java.util.Scanner;                           
+
+/**Tamaños de cazuelas
+int pequeña = 16*10;
+float mediana = (float) (20*11.5);
+float grande = (float) (24*13.5);
+*/
+/**Tamaño huevo
+int huevo = 6*4;
+*/
+
+public class Ejercicio_5                    
+{
+    public static void main(String[] args)
+    {   
+
+
+
+
+    }
+}
