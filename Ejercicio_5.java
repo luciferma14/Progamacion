c70c08fd1635b2f1a4d7b17e43d93ed2f31bc147
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-09-28 19:25:28 +0200

    Add files via upload

--- Ejercicio_5.java
+++ Ejercicio_5.java
@@ -1,21 +1,37 @@
 package EjerciciosAvanzados1;
-import java.util.Scanner;                           
 
-/**Tamaños de cazuelas
-int pequeña = 16*10;
-float mediana = (float) (20*11.5);
-float grande = (float) (24*13.5);
-*/
-/**Tamaño huevo
-int huevo = 6*4;
-*/
+
+import java.util.Scanner;                           
 
 public class Ejercicio_5                    
 {
     public static void main(String[] args)
     {   
+        Scanner sc = new Scanner (System.in);
 
+        System.out.println("Cuantos huevos caben en la olla: ");
 
+        double olla;
+
+        olla = sc.nextInt();
+        
+        sc.nextLine();
+
+        System.out.println("Cuantos huevos quieres meter en la olla: ");
+        
+        double huevos;
+
+        huevos = sc.nextInt();
+
+        sc.nextLine();
+
+        double redondeo = Math.ceil(huevos/olla);
+
+        int tiempo = (int) (redondeo * 10);
+
+        System.out.println("Tardará en hacerlos: " + tiempo + " minutos");
+
+    
 
 
     }
