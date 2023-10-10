1ed3bbcc9c6b77afe3885144a1bbe685488de819
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-10-10 20:01:16 +0200

    Add files via upload

---
+++ EjercicioPE_1.java
@@ -0,0 +1,10 @@
+public class EjercicioPE_1 {
+    public static void main(String[] args) {
+
+        for (int i = 0 ; i <= 100; i++){
+            if (i % 5 == 0)
+            
+            System.out.println(i);
+        }
+    } 
+}
