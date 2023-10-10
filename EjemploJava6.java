b752a23aa9a0f5b5c28c582f6c48b066d443e0b2
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-10-10 19:47:56 +0200

    Add files via upload

---
+++ EjemploJava6.java
@@ -0,0 +1,10 @@
+public class EjemploJava6 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 50; i++){
+
+            System.out.println(i);
+
+        }
+    }
+}            
