ceb5f2159921360d9eb1d64e45e41e629cc06382
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-10-18 11:14:31 +0200

    Add files via upload

--- EjercicioPE_7.java
+++ EjercicioPE_7.java
@@ -5,40 +5,55 @@ public class EjercicioPE_7 {
 
         Scanner sc = new Scanner(System.in);
 
-        int piedra = 1;
-        int papel = 2;
-        int tijera = 3;
-        int opciones;
-        int aleatorio;
+        String opcion1 = "piedra";
+        String opcion2 = "papel";
+        String opcion3 = "tijera";
+        int opcionOrdenador;
+        int opcionUsuario;
+        String opcionOrdenadorStg;
+        String opcionUsuarioStg;
 
-        System.out.print("El ordenador a sacado: ");
 
-        aleatorio = (int)Math.floor(Math.random()*3+1);
-        System.out.println(aleatorio);
+        opcionOrdenador = (int) (Math.random() * 3) + 1;
 
-        System.out.print("Tu sacas: ");
-        opciones = sc.nextInt();
+        System.out.println("Juego Piedra, Papel o Tijera");
+        System.out.println("Elige una opción: ");
+        System.out.println("1 - Piedra");
+        System.out.println("2 - Papel");
+        System.out.println("3 - Tijera");
+       
 
-        switch (opciones) {
-            case 1: System.out.println("Piedra");break;
-            case 2: System.out.println("Papel");break;
-            case 3: System.out.println("Tijeras");break;
-        }    
-        
-        if (piedra == 1) {
-            System.out.println("Ha habido un empate");
+        do {
+            System.out.print("Introduce tu opción: ");
+             opcionUsuario = sc.nextInt();
 
-        } else if (papel == 2) {
-            System.out.println("Ha habido un empate");
+        } while (opcionUsuario < 1 || opcionOrdenador > 3);
 
-        }else if (tijera == 3) {
-            System.out.println("Ha habido un empate");
-
+        switch (opcionOrdenador) {
+            case 1: opcionOrdenadorStg = "Piedra"; break;
+            case 2: opcionOrdenadorStg = "Papel"; break;
+            case 3: opcionOrdenadorStg = "Tijera"; break;
         }
+
+        switch (opcionUsuario) {
+            case 1: opcionUsuarioStg = "Piedra"; break;
+            case 2: opcionUsuarioStg = "Papel"; break;
+            case 3: opcionUsuarioStg = "Tijera"; break;
+        }
+
         
+        System.out.println("El ordenador eligió: " + opcionOrdenador);
 
 
+        if (opcionOrdenador == opcionUsuario) {
+            System.out.println("Ha habido un empate!");
 
+        } else if ((opcionUsuario == 1 && opcionOrdenador == 3) || (opcionUsuario == 2 && opcionOrdenador == 1) || (opcionUsuario == 3 && opcionOrdenador == 2)) {
+            System.out.println("HAS GANADO!! :)");
+
+        } else {
+            System.out.println("HAS PERDIDO!! :(");
+        }
 
 
     }
