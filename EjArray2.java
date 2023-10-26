cf8ebc87128eacef8e95e450a649c89ca36efde2
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-10-26 16:53:36 +0200

    Add files via upload

---
+++ EjArray2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class EjArray2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String jugador = "X";
+        String ordenador = "O";
+
+        
+        
+
+
+
+
+
+    }
+}    
