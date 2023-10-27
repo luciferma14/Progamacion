04e0275ae915c345764bbe149206a3b5e6b621c7
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-10-27 16:53:59 +0200

    Add files via upload

--- EjArray2.java
+++ EjArray2.java
@@ -9,12 +9,27 @@ public class EjArray2 {
         String jugador = "X";
         String ordenador = "O";
 
+        char[][]a = {{'X'},{'O'},{'X'}};;
+                      
+
+        for (int i = 1; i <=1; i++){
+            for (int j = 2; j <=2; j++){
+                for (int z = 3; z <= 3; z++){
+                    
+                    System.out.println("   " + i + "  " + j + "  " + z);
+                                
+                }
+            }
+        }
         
+
+        System.out.println("1| " + " | " + " | " + " | ");
+        System.out.println(" ----------");
+        System.out.println("2| " + " | " + " | " + " | ");
+        System.out.println(" ----------");
+        System.out.println("3| " + " | " + " | " + " | ");
         
-
-
-
-
+        System.out.println("Empieza el jugador, elige una posición (indicando fila/columna): ");
 
     }
 }    
