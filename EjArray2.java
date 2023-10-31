5803602a8be451ab511aa798223d868d607b12c3
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-10-31 12:47:12 +0100

    Add files via upload

--- EjArray2.java
+++ EjArray2.java
@@ -8,6 +8,8 @@ public class EjArray2 {
 
         String jugador = "X";
         String ordenador = "O";
+        int fila;
+        int columna;
 
         char[][]a = {{'X'},{'O'},{'X'}};;
                       
@@ -22,14 +24,143 @@ public class EjArray2 {
             }
         }
         
-
         System.out.println("1| " + " | " + " | " + " | ");
         System.out.println(" ----------");
         System.out.println("2| " + " | " + " | " + " | ");
         System.out.println(" ----------");
         System.out.println("3| " + " | " + " | " + " | ");
-        
-        System.out.println("Empieza el jugador, elige una posición (indicando fila/columna): ");
+
+
+        System.out.println("Empieza el jugador, elige una posición (indicando columna/fila): ");
+        columna = sc.nextInt();
+        fila = sc.nextInt();
+
+        //for (fila = 1; fila <= 1; fila++) {
+        //    for (columna = 1; columna <= 1; columna++) {
+
+        //        System.out.println(jugador);
+        //    }
+        //    }
+
+
+        if (columna == 1) {
+            if (fila == 1) {
+
+                System.out.println("   1   2   3");
+                System.out.println("1| " + "X"+ " | " + "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("2| " + "  | " + "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("3| " + "  | " + "  | " + "  | ");
+            }
+
+        }
+            if (columna == 1) {
+                if (fila == 2) {
+
+                System.out.println("   1   2   3");
+                System.out.println("1| " +  "  | " + "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("2| " + "X"+" | " + "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("3| " + "  | " + "  | " + "  | ");
+            }
+
+        }
+
+                if (columna == 1) {
+                    if (fila == 3) {
+
+                System.out.println("   1   2   3");
+                System.out.println("1| " +  "  | " + "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("2| " + "  | " + "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("3| " +  "X"+ " | " + "  | " + "  | ");
+        }
+
 
     }
-}    
+
+                if (columna == 2) {
+                    if (fila == 1) {
+
+                System.out.println("   1   2   3");
+                System.out.println("1| " +  "  | " +  "X"+" | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("2| " + "  | " + "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("3| " +  "  | " + "  | " + "  | ");
+
+                }
+            }
+
+                if (columna == 2) {
+                    if (fila == 2) {
+
+                System.out.println("   1   2   3");
+                System.out.println("1| " +  "  | " + "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("2| " + "  | " +   "X"+" | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("3| " +  "  | " + "  | " + "  | ");
+
+                }
+            }
+
+                if (columna == 2) {
+                    if (fila == 3) {
+
+                System.out.println("   1   2   3");
+                System.out.println("1| " +  "  | " + "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("2| " + "  | " +   "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("3| " +  "  | " + "X"+" | " + "  | ");
+
+                }
+            }
+
+                if (columna == 3) {
+                    if (fila == 1) {
+
+                System.out.println("   1   2   3");
+                System.out.println("1| " +  "  | " + "  | " + "X"+" | ");
+                System.out.println(" -------------");
+                System.out.println("2| " + "  | " +   "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("3| " +  "  | " +  "  | " + "  | ");
+
+                }
+            }
+
+                if (columna == 3) {
+                    if (fila == 2) {
+
+                System.out.println("   1   2   3");
+                System.out.println("1| " +  "  | " + "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("2| " + "  | " +   "  | " + "X"+" | ");
+                System.out.println(" -------------");
+                System.out.println("3| " +  "  | " +  "  | " + "  | ");
+
+                }
+            }
+
+                if (columna == 3) {
+                    if (fila == 3) {
+
+                System.out.println("   1   2   3");
+                System.out.println("1| " +  "  | " + "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("2| " + "  | " +   "  | " + "  | ");
+                System.out.println(" -------------");
+                System.out.println("3| " +  "  | " +  "  | " + "X"+" | ");
+
+                }
+            }
+            
+    }
+    }
+
+    
