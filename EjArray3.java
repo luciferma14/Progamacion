b3c97f12f0388ff6e3f4b670bddc9d2e96eec4f2
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-11-03 16:16:13 +0100

    Add files via upload

--- EjArray3.java
+++ EjArray3.java
@@ -5,28 +5,39 @@ public class EjArray3 {
 
         Scanner sc = new Scanner(System.in);
 
-        int fila1;
-        int columna1;
-        int i = 0;
+        boolean simetrica;
+        
 
-        char[][] a ={{' ',' ',' ',' '},{' ',' ',' ',' '},{' ',' ',' ',' '},{' ',' ',' ',' '}};
+        int[][] matriz = new int[4][4];
 
-            //Mostrar matriz
-            System.out.println("   0   1   2   3  ");
-            for (int ia = 0; ia < 4; ia++) {
+            System.out.println("Introduce los números de la matriz 4x4: ");
+
+            for (int i = 0; i < 4; i++) {
                 for (int j = 0; j < 4; j++) {
-                    if (j <= 0) {
-                        System.out.print(ia + "| ");
-                    }
-                    System.out.print(a[ia][j]);
-                    if (j < 4) {
-                        System.out.print(" | ");
-                    }
-                }
-                System.out.println();
-                if (ia < 4) {
-                    System.out.println("------------------");
+                    System.out.print("m[" + i + "][" + j + "]: ");
+                    matriz[i][j] = sc.nextInt();
                 }
             }
+
+            simetrica = true;
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                     if (matriz[i][j] != matriz[j][i]) {
+                        simetrica = false;
+                     }break;
+                }
+            }
+            
+            System.out.println("La matriz es:");
+            for(int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    System.out.println(matriz[i][j] + " ");
+                }
+            }
+
+            if (simetrica) {
+                System.out.println("Es simétrica");
+            }else 
+                System.out.println("No es simétrica");
     }
 }
