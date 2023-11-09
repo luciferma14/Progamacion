5e34248cc3dd601adc1fa52fd22f9c3f0868d464
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-11-09 16:42:00 +0100

    Add files via upload

---
+++ Ex2Ej1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex2Ej1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int casos;
+        int clientes;
+        int maletas;
+
+        System.out.print("Cuantos casos quieres resolver: ");
+        casos = sc.nextInt();
+        System.out.print("Cuantos clientes han llegado: ");
+        clientes = sc.nextInt();
+        System.out.print("Cuantas maletas tiene cada cliente: ");
+        maletas = sc.nextInt();
+
+
+
+
+
+    }
+}
