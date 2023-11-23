851c2d47ea2fdcb52b7208b56eaa57fcb3e1853c
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-11-23 16:20:51 +0100

    Add files via upload

--- AER210.java
+++ AER210.java
@@ -1,42 +1,31 @@
-//import libreria.FNumDAW;
+import libreria.FNumDAW;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class AER210 {
-    public static void main (String[] args){
+    public static void main (String[] args) {
 
-        Scanner s = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        //int casos;
-        //String valores = "";
-//
+        int casos;
+        String valores = "";
+        int suma;
 
-        //System.out.print("Casos: ");
-        //casos = sc.nextInt();
-
-        /*for (int i = 0; i < casos; i++){
-            List<Integer> listaIntegers = valores.chars().boxed().collect(Collectors.toList());       
-            valores = sc.next();
-            for (int b = 0; b < valores.length(); b++){
-                for (int j : listaIntegers) {
-                    System.out.println(j);
-                }
-            }           
-        }*/
-        int casos = s.nextInt();
-        s.nextLine();
-        int suma, output;
-        String nombre;
-
-        for (int i = 0; i < casos; i++) {
-
-            nombre = s.nextLine();
-
+        System.out.print("Casos: ");
+        casos = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < casos; i++){
             suma = 0;
-            for (int j = 0; j < nombre.length(); j++) {
-                suma += nombre.charAt(j);
+            valores = sc.nextLine();
+            List<Integer> listaIntegers = valores.chars().boxed().collect(Collectors.toList());       
+            for (int j : listaIntegers) {
+                suma = suma + j;
+
             }
+            
+            System.out.println(FNumDAW.anteriorPrimo(suma));
+
         }
     }
 }
