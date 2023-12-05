67b2961b2c2d10be153268508dfb6c58b5c66483
luciferma14 <145020049+luciferma14@users.noreply.github.com>
2023-12-05 11:56:36 +0100

    Add files via upload

---
+++ AER_369.java
@@ -0,0 +1,28 @@
+package AER;
+
+import java.util.Scanner;
+
+public class AER_369 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        
+        while (true){
+            System.out.print("Numero: ");
+            num = sc.nextInt();
+            
+            for (int i = 0; i < num; i++){
+                System.out.print("1");
+            }
+
+            if (num == 0){
+                System.exit(0);
+            }
+            System.out.println();
+        }
+
+
+    }
+}
