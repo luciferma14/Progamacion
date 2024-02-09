5eb60c0627591b33f88c089b1691ea63f5aa871d
Lucia Ferrandis <luciferma14@gmail.com>
2024-02-09 12:38:36 +0100

    Update

--- POO/MenuVehiculos.java
+++ POO/MenuVehiculos.java
@@ -5,30 +5,54 @@ public class MenuVehiculos {
 
         Scanner sc = new Scanner(System.in);
 
+        Bicicleta bici = new Bicicleta();
+        Coche carro = new Coche();
+
         int opc;
         
-        System.out.println("              VEHÍCULOS"); 
-        System.out.println("====================================="); 
-        System.out.println("1. Anda en bicicleta"); 
-        System.out.println("2. Anda en coche"); 
-        System.out.println("3. Ver kilometraje de la bicicleta"); 
-        System.out.println("4. Ver kilometraje del coche"); 
-        System.out.println("5. Ver kilometraje total"); 
-        System.out.println("6. Ver vehículos totales");
-        System.out.println("7. Salir");
-        System.out.print("Elige una opción (1-7): ");
-        opc = sc.nextInt();
+        do{
+            System.out.println("              VEHICULOS"); 
+            System.out.println("====================================="); 
+            System.out.println("1. Anda en bicicleta"); 
+            System.out.println("2. Anda en coche"); 
+            System.out.println("3. Ver kilometraje de la bicicleta"); 
+            System.out.println("4. Ver kilometraje del coche"); 
+            System.out.println("5. Ver kilometraje total"); 
+            System.out.println("6. Ver vehículos totales");
+            System.out.println("7. Salir");
+            System.out.print("Elige una opción (1-7): ");
+            opc = sc.nextInt();
+            System.out.println(" ");
 
-        switch (opc) {
-            case 1:
-                
-                break;
-            case 2:
-
-            default:
-                break;
-        }
-
-        
+            switch (opc) {
+                case 1:
+                    bici.anda(10);
+                    System.out.println("Se han sumado 10 km");
+                    System.out.println(" ");
+                    break;
+                case 2:
+                    carro.anda(100);
+                    System.out.println("Se han sumado 100 km");
+                    System.out.println(" ");
+                    break;
+                case 3:
+                    System.out.println("Km Bici: " + bici.getKilometrosRecorridos());
+                    break;
+                case 4:
+                    System.out.println("Km Coche: " + carro.getKilometrosRecorridos());
+                    break;
+                case 5:
+                    System.out.println("Km Totales: " + Vehiculo.getKilometrosTotales());
+                    break;
+                case 6:
+                    System.out.println("Vehiculos: " + Vehiculo.getVehiculosCreados());
+                    break;
+                case 7:
+                    System. exit(0);
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
+        }while(true);
     }
 }
--- POO/Vehiculo.java
+++ POO/Vehiculo.java
@@ -1,6 +1,27 @@
 public class Vehiculo {
     
     public static int vehiculosCreados = 0;
-    public static double kilometrosTotales = 0;
-    public static double kilometrosRecorridos = 0;
+    public static int kilometrosTotales = 0;
+    public static int kilometrosRecorridos = 0;
+
+   
+    public int anda(int km) {
+        vehiculosCreados++;
+        kilometrosRecorridos += km;
+        kilometrosTotales += km;
+        return kilometrosRecorridos;
+    }
+
+    public static int getVehiculosCreados() {
+        return vehiculosCreados;
+    }
+
+    public static int getKilometrosTotales() {
+        return kilometrosTotales;
+    }
+
+    public static int getKilometrosRecorridos() {
+        return kilometrosRecorridos;
+    }
+    
 }   
