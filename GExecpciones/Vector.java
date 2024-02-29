6a031b626585f444881655f119aa6c303154da46
Lucia Ferrandis <luciferma14@gmail.com>
2024-02-29 19:32:53 +0100

    Update

--- GExecpciones/Vector.java
+++ GExecpciones/Vector.java
@@ -1,28 +1,28 @@
-
+package GExecpciones;
 import java.util.Scanner;
 
-public class Vector{
+public class Vector {
     public static void main(String[] args) {
-    
+
         Scanner sc = new Scanner(System.in);
 
-        int vector[] = {0,1,2,3,4};
-    
+        int[] vector = {0,1,2,3,4};
+
         for (int i = 0; i < vector.length; i++) {
             vector[i] = i;
         }
-        try {
-            System.out.print("Añade valor: ");
-            int valor = sc.nextInt();
 
-            if (valor < 0 || valor > vector.length - 1) {
-                throw new ArrayIndexOutOfBoundsException("Valor fuera del rango");
-            }
+        try {
+            System.out.print("Posición: ");
+            int pos = sc.nextInt();
+            System.out.print("Número que añades: ");
+            int num = sc.nextInt();
+            vector[pos] = num;
+
+            System.out.println("Valor añadido");
+
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Error: Estás fuera del vector");
-        }
-        for (int i = 0; i < vector.length; i++) {
-            System.out.println(i);
+            System.out.println("Error: La posición está fuera del rango");
         }
     }
 }
