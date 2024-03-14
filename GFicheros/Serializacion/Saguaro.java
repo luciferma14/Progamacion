66da6611a733cc280e23441ae6a9f55cf1f26efa
Lucia <luciferma14@gmail.com>
2024-03-14 16:53:11 +0100

    Update

--- GFicheros/Serializacion/Saguaro.java
+++ GFicheros/Serializacion/Saguaro.java
@@ -1,10 +1,37 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileInputStream ;
 
 public class Saguaro {
     public static void main(String[] args) {
-        File file = new File("saguarGalaxies.TXT");
 
-        
+        try{
+            File file = new File("saguaroGalaxies.TXT");
+
+            String[] lista = file.list();
+
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String linea = br.readLine();
+
+            while (linea != null) {
+
+                String[] coma = linea.split(",");
+
+                linea = linea.replaceAll(" ", "").replaceAll(";", ",").replaceAll("\"", " ");
+
+                coma[2].equals("GALXY");
+
+                System.out.println(coma[0] + " ; " + coma[3] + " ; " +  coma[4] + " ; " +  coma[5] + " ; " +  coma[6]);
+               
+
+                linea = br.readLine();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 
 
 
