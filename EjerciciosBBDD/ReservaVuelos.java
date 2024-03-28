3798297e66251f6fd99931c19d54af767abfd5e8
Lucia Ferrandis <luciferma14@gmail.com>
2024-03-28 18:53:56 +0100

    Update

--- EjerciciosBBDD/ReservaVuelos.java
+++ EjerciciosBBDD/ReservaVuelos.java
@@ -9,23 +9,24 @@ public class ReservaVuelos{
 
         try{
 
-            String vuelos;
-
+            int vuelos;
+            int pasajeros;
+            int vuelosPasajeros;
+            
             //Conexión con la BD
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ReservaVuelos","lucia", "lucia");
+            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/reservavuelos","lucia","lucia");
 
             //Connection con = DriverManager.getConnection("jdbc:mysql://localhost:33006/ReservaVuelos","root", "dbrootpass");
              // Creamos un objeto para enviar sentencias SQL a la BD
             Statement st = con.createStatement();
              // Ejecutamos la consulta SQL y obtenemos el resultado en ResultSet
-            //ResultSet rs = st.executeQuery("SELECT NOM, CLIENT_COD FROM CLIENT");
+            ResultSet rs = st.executeQuery("SELECT * FROM vuelos");
 
-            // vuelos =  "CREATE TABLE(" + "id_vuelo INT PRIMARY KEY," + "numero_vuelo INT NOT NULL," + "origen VARCHAR(50) NOT NULL," + "destino VARCHAR(50) NOT NULL," + "fecha DATE NOT NULL," + "capacidad INT NOT NULL)";
-            // st.executeUpdate(vuelos);
+            vuelos = st.executeUpdate("INSERT INTO uelos (id_vuelo, numero_vuelo, origen, destino, fecha, capacidad) VALUES (12345, 'AA123', 'Valencia', 'Madrid', 2023-03-28, 180)");
+            pasajeros = st.executeUpdate("INSERT INTO pasajeros (id_pasajeros, numero_pasaporte, nombre_pasajero) VALUES (67890, '111111', 'Álvaro')");
+            vuelosPasajeros = st.executeUpdate("INSERT INTO vuelos_pasajeros (id_vuelo, id_pasajero, n_asiento) VALUES (12345, 67890, '180')");
 
-
-
-
+            con.close();
         }catch (SQLException e) {
             System.out.println("Error en la bd: " + e.getErrorCode() + "-" + e.getMessage());
         }catch (Exception e){
