f98d55162376265b93686978be5899c5c0ab2d9e
Lucia <luciferma14@gmail.com>
2024-04-11 15:29:11 +0200

    Update

---
+++ ProyectoJAVA/Libros.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class Libros {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private String genero;
+
+    public static List<Libros>buscar(String criterio){
+
+        //Consultar en la base de datos los libros que coincidan con el criterio
+        return null; //De momento
+    }
+
+    public void reservar(){
+
+        //Marcar el libro como reservado en la base de datos
+    }
+
+    public void prestar(Usuarios usuario){
+
+        //Registrar el préstamo del libro al usuario en la base de datos
+    }
+
+    public void devolver(){
+
+        //Registrar la devolución del libro en la base de datos
+    }
+}
---
+++ ProyectoJAVA/Usuarios.java
@@ -0,0 +1,13 @@
+public class Usuarios {
+    private int id;
+    private String nombre;
+    private String password;
+    private boolean admin;
+
+    public Usuarios (int id, String nombre, String password, boolean admin){
+        this.id = id;
+        this.nombre = nombre;
+        this.password = password;
+        this.admin = admin;
+    }
+}
