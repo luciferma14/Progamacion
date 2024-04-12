f06e8592741da354eb2dd9f8f42713148dd01cf4
Lucia Ferrandis <luciferma14@gmail.com>
2024-04-12 16:14:59 +0200

    Update

--- ProyectoJAVA/proyecto_java/src/main/java/luciferma_jfx/Usuarios.java
+++ ProyectoJAVA/proyecto_java/src/main/java/luciferma_jfx/Usuarios.java
@@ -1,5 +1,9 @@
 package luciferma_jfx;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Usuarios {
     private int id;
     private String nombre;
@@ -10,6 +14,12 @@ public class Usuarios {
 
     public static Usuarios login(String email, String password){
         //Consultar en la base de datos el usuario con el email y password
+        try{
+            Connection con = ConexionBD.getConnection();
+
+            String sql = "SELECT * FROM usuarios WHERE email = ? AND password = ?";
+            
+        }
         return null; //de momento
     }
 
@@ -19,5 +29,24 @@ public class Usuarios {
 
     public void modPerfil(){
         //Actualizar la información del usuario en la base de datos
+    }
+
+
+    //conexión con la base de datos
+    public static class ConexionBD {
+
+        private static final String conex= "jdbc:mysql://localhost:3306/tu_base_de_datos";
+        private static final String usu = "tu_usuario";
+        private static final String contra = "tu_contraseña";
+    
+        public static Connection getConnection() throws SQLException {
+            return DriverManager.getConnection(conex, usu, contra);
+        }
+    
+        public static void closeConnection(Connection conn) throws SQLException {
+            if (conn != null) {
+                conn.close();
+            }
+        }
     }
 }
--- ProyectosJFX/demo/src/main/java/module-info.java
+++ ProyectosJFX/demo/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module luciferma_jfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens luciferma_jfx to javafx.fxml;
     exports luciferma_jfx;
