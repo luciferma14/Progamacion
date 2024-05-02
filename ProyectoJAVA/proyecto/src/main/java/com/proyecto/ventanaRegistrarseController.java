d2006ec82f40c2e51c01282547c36b2b44941f87
Lucia <luciferma14@gmail.com>
2024-05-02 16:48:42 +0200

    Update

--- ProyectoJAVA/proyecto/src/main/java/com/proyecto/ventanaRegistrarseController.java
+++ ProyectoJAVA/proyecto/src/main/java/com/proyecto/ventanaRegistrarseController.java
@@ -7,16 +7,19 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 public class ventanaRegistrarseController {
 
-    @FXML
-    private void cambiaAPrimeraVentana() throws IOException {
-        App.setRoot("primeraVentana");
-    }
+	private static String driv = "com.mysql.jdbc.Driver";
+    private static String bibl = "jdbc:mysql://localhost:33006/Biblioteca";
+    private static String usr= "root";
+    private static String pass = "dbrootpass";	
 
-    @FXML
+	@FXML
+	private ComboBox idRol;
+	@FXML
 	private TextField idUser;
 	@FXML
 	private TextField idPass;
@@ -24,12 +27,19 @@ public class ventanaRegistrarseController {
 	private TextField idNombre;
 	@FXML
 	private TextField idApellido;
+	
 
-    private static String driv = "com.mysql.jdbc.Driver";
-    private static String bibl = "jdbc:mysql:33006/biblioteca";
-    private static String usr= "root";
-    private static String pass = "dbrootpass";	
+	@FXML
+	public void initialize() {
+		idRol.getItems().addAll("Usuario", "Administrador");
+	}
 
+    @FXML
+    private void cambiaAPrimeraVentana() throws IOException {
+        App.setRoot("primeraVentana");
+    }
+
+	
 
 	@FXML
 	public int registrar(ActionEvent event) throws IOException, SQLException {
@@ -44,15 +54,16 @@ public class ventanaRegistrarseController {
 			} catch (ClassNotFoundException e) {
 				System.out.println(e.getMessage());
 			}
-				
-			String query = "INSERT INTO usuarios(email, apellido, nombre, password, idRol) VALUES(?, ?, ?, ?)";
+			
+			String query = "INSERT INTO usuarios(email, apellido, nombre, password, idRol) VALUES(?, ?, ?, ?, ?)";
 
 			try (PreparedStatement st = con.prepareStatement(query)) {
 
-				st.setString(1, String.valueOf(idUser));
-				st.setString(2, String.valueOf(idApellido));
-				st.setString(3, String.valueOf(idNombre));
-				st.setString(4, String.valueOf(idPass));
+				st.setString(1, String.valueOf(idUser.getText()));
+				st.setString(2, String.valueOf(idApellido.getText()));
+				st.setString(3, String.valueOf(idNombre.getText()));
+				st.setString(4, String.valueOf(idPass.getText()));
+				st.setString(5, String.valueOf(idRol.getPromptText()));
 		
 				st.executeUpdate();
 
