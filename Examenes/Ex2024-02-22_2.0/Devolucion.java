62eec67d188a24607a28e02b478f592b44fcca80
Lucia Ferrandis <luciferma14@gmail.com>
2024-06-03 19:41:08 +0200

    Update

--- Examenes/Ex2024-02-22_2.0/Devolucion.java
+++
@@ -1,9 +0,0 @@
-
-public class Devolucion {
-    public Devolucion(String cliente, String producto, float precio){
-        super(cliente, producto, precio); 
-        this.id = "DE" + this.id;
-    }
-
-    
-}
