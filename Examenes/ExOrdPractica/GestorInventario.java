d4eff8153009182544bacfe71ed111fd2cd59410
Lucia Ferrandis <luciferma14@gmail.com>
2024-06-12 18:57:52 +0200

    Update

--- Examenes/ExOrdPractica/GestorInventario.java
+++ Examenes/ExOrdPractica/GestorInventario.java
@@ -8,16 +8,29 @@ import java.io.IOException;
 public class GestorInventario {
     
     public static void main(String[] args) {
+
+        Inventario inventario = new Inventario();
         
         try {
             File f = new File("productos_in.txt");
             FileReader fr = new FileReader(f);
             BufferedReader br = new BufferedReader(fr);
+
             String linea = br.readLine(); //fr.read(), para un caracter
             System.out.println();
-            while(linea != null) {
+            String [] partes = linea.split(",");
+
+            while(partes != null) {
                 System.out.println(linea);
                 linea = br.readLine();
+                String nom = partes[0];
+                double precio = Double.parseDouble(partes[1]);
+                String categ = partes[2];
+
+                Producto prod = new Producto(nom, precio, categ);
+
+                inventario.agregarProducto(prod);
+                
             }
             br.close();
             fr.close();
