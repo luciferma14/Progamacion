d61091679f98e0bd686f616383aa37742cb29614
Lucia Ferrandis <luciferma14@gmail.com>
2024-06-12 18:20:42 +0200

    Update

---
+++ Examenes/ExOrdPractica/GestorInventario.java
@@ -0,0 +1,28 @@
+package ExOrdPractica;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class GestorInventario {
+    
+    public static void main(String[] args) {
+        
+        try {
+            File f = new File("productos_in.txt");
+            FileReader fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            String linea = br.readLine(); //fr.read(), para un caracter
+            System.out.println();
+            while(linea != null) {
+                System.out.println(linea);
+                linea = br.readLine();
+            }
+            br.close();
+            fr.close();
+            } catch(IOException e) {
+                e.printStackTrace();
+        }
+    }
+}
