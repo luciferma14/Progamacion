fb1d35c404b40f662a2a4bb9d865fd9a879535e1
Lucia <luciferma14@gmail.com>
2024-06-13 16:55:40 +0200

    Update

---
+++ AER/AER_433.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AER_433 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> uvas = new ArrayList<Integer>();
+
+        int uva;
+
+        do {
+            uvas = new ArrayList<Integer>();
+            uva = sc.nextInt();
+
+        }while(uva != 0);
+
+
+
+    }
+}
